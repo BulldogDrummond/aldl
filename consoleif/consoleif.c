711687a626ec7cce6adee7f88a7b356f96e7548d
resfilter <resfilter@resfilter.net>
2013-10-23 21:04:07 -0700

    add two demo gauges

--- consoleif/consoleif.c
+++ consoleif/consoleif.c
@@ -17,7 +17,16 @@ enum {
   WHITE_ON_BLACK = 5
 };
 
+typedef struct _gauge {
+  int x, y; /* coords */
+  int width, height; /* size */
+  int data_a, data_b; /* assoc. data index */
+  aldl_data_t prev_a, prev_b; /* prev. value */
+  float bottom, top; /* bottom and top of a graph */
+} gauge_t;
+
 #define COLOR_STATUSSCREEN RED_ON_BLACK
+#define COLOR_PROGRESSBAR RED_ON_BLACK
 
 /* --- variables ---------------------------- */
 
@@ -42,6 +51,8 @@ void statusmessage(char *str);
 /* clear screen and display waiting for connection messages */
 void cons_wait_for_connection();
 
+void draw_h_progressbar(gauge_t *g);
+
 /* --------------------------------------------*/
 
 void *consoleif(void *aldl_in) {
@@ -54,6 +65,7 @@ void *consoleif(void *aldl_in) {
     fatalerror(ERROR_NULL,"could not init ncurses");
   };
 
+  curs_set(0);
   start_color();
   init_pair(RED_ON_BLACK,COLOR_RED,COLOR_BLACK);
   init_pair(BLACK_ON_RED,COLOR_BLACK,COLOR_RED);
@@ -71,14 +83,30 @@ void *consoleif(void *aldl_in) {
   int rpmid = get_index_by_name(aldl,"NEWRFPRT");
   if(rpmid == -1) fatalerror(ERROR_NULL,"rpm not found");
 
+  gauge_t *demogauge = malloc(sizeof(gauge_t));
+  demogauge->x = 1;
+  demogauge->y = 1;
+  demogauge->width=30;
+  demogauge->data_a = get_index_by_name(aldl,"ISESDD");
+  demogauge->bottom = 0;
+  demogauge->top = 3187.50;
+
+  gauge_t *rpmgauge = malloc(sizeof(gauge_t));
+  rpmgauge->x = 1;
+  rpmgauge->y = 4;
+  rpmgauge->width = 30;
+  rpmgauge->data_a = get_index_by_name(aldl,"NTRPMX");
+  rpmgauge->bottom = 0;
+  rpmgauge->top = 6375;
+
   while(1) {
     rec = newest_record_wait(aldl,rec);
-    sprintf(bigbuf,"RPM: %.2f",rec->data[rpmid].f);
-    mvaddstr(ycenter(0),xcenter(strlen(bigbuf)),bigbuf);
-    sprintf(bigbuf,"TIMESTAMP: %i",rec->t);
+    sprintf(bigbuf,"TIMESTAMP: %i",(int)rec->t);
     mvaddstr(ycenter(0)+1,xcenter(strlen(bigbuf)),bigbuf);
+    draw_h_progressbar(demogauge);
+    draw_h_progressbar(rpmgauge);
     refresh();
-
+    usleep(10000);
   };
 
   sleep(4);
@@ -121,3 +149,26 @@ void cons_wait_for_connection() {
 
   clear();
 }
+
+void draw_h_progressbar(gauge_t *g) {
+  aldl_define_t *def = &aldl->def[g->data_a];
+  float data = rec->data[g->data_a].f;
+  /* draw title text */
+  mvaddstr(g->y,g->x,def->name);
+  /* draw output text */
+  sprintf(bigbuf,"%.1f %s",data,def->uom);
+  mvaddstr(g->y,g->x + g->width - strlen(bigbuf),bigbuf);
+  /* draw progress bar */
+  move(g->y + 1, g->x);
+  int filled = data / ( g->top / g->width );
+  int remainder = g->width - filled;
+  int x;
+  attron(COLOR_PAIR(COLOR_PROGRESSBAR)); 
+  for(x=0;x<filled;x++) { /* draw filled section */
+    addch(' '|A_REVERSE);
+  }; 
+  for(x=0;x<remainder;x++) { /* draw unfilled section */
+    addch('-');
+  };
+  attroff(COLOR_PAIR(COLOR_PROGRESSBAR));
+};
