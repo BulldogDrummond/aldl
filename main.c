901c6b7d841c5f56a0d4476d73f4653cfb3fc000
resfilter <resfilter@resfilter.net>
2013-10-25 09:40:26 -0700

    add command line plugin selection override (+ only)

--- main.c
+++ main.c
@@ -22,7 +22,7 @@
 /* run cleanup rountines for aldl and serial crap */
 int aldl_finish();
 
-int main() { /*-------------------------------------------------- */
+int main(int argc, char **argv) { /*--------------------------- */
 
   /*------ INITIALIZE ----------------------------------------------*/
 
@@ -30,8 +30,30 @@ int main() { /*-------------------------------------------------- */
   init_locks();
 
   /* allocate everything and parse config */
-  /* FIXME this static config file thing has to go */
   aldl_conf_t *aldl = aldl_setup();
+
+  /* ----- PROC CMDLINE OPTS ---------------------------------------*/
+
+  int n_arg = 0;
+  for(n_arg=0;n_arg<argc;n_arg++) {
+    if(faststrcmp(argv[n_arg],"configtest") == 1) {
+      printf("Loaded config OK.  Exiting...\n");
+      exit(0);
+    } else if(faststrcmp(argv[n_arg],"debugif") == 1) {
+      aldl->debugif_enable = 1;
+    } else if(faststrcmp(argv[n_arg],"consoleif") == 1) {
+      aldl->consoleif_enable = 1;
+    } else if(faststrcmp(argv[n_arg],"datalogger") == 1) {
+      aldl->datalogger_enable = 1;
+    };
+  };
+
+  /* compatibility checking */
+  if(aldl->debugif_enable == 1 && aldl->consoleif_enable == 1) {
+    fatalerror(ERROR_PLUGIN,"consoleif and debugif plugins are incompat");
+  };
+
+  /* ---- MORE INIT -----------------------------------------------*/
 
   /* initial record so linked list isnt broken */
   aldl_init_record(aldl);
@@ -43,11 +65,6 @@ int main() { /*-------------------------------------------------- */
   serial_init(aldl->serialstr);
 
   /* ------ THREAD SPAWNING -------------------------------------*/
-
-  /* error checking */
-  if(aldl->debugif_enable == 1 && aldl->consoleif_enable == 1) {
-    fatalerror(ERROR_PLUGIN,"consoleif and debugif plugins are incompat");
-  };
 
   /* configure threading */
   pthread_t thread_acq; 
