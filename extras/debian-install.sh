4645a517ac62638d117ef90987011dff349b6a18
resfilter <resfilter@resfilter.net>
2013-11-09 07:42:31 -0800

    clean up debian install file

--- extras/debian-install.sh
+++ extras/debian-install.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
 
-echo "adding usb group"
+echo "This script is for modern debian systems, and does everything that should"
+echo "be necessary to use ftdi usb devices as a regular user with libftdi,"
+echo "instead of the built-in dumb kernel driver."
+
+if [ $(id -u) != 0 ]; then
+  echo "This script must be run as root."
+  exit 1
+else
+  read -p "Press enter to continue, or ctrl-c to bail..." x
+fi
+
+echo "** Adding usb group"
 groupadd usb
 
-echo "setting permissions on usb devices"
+echo "** Setting udev permissions on usb devices"
 echo 'SUBSYSTEMS=="usb", ACTION=="add", MODE="0664", GROUP="usb"' >> /etc/udev/rules.d/30-usb.rules
 
-echo "blacklisting ftdi_sio driver"
+echo "** Blacklisting ftdi_sio driver"
 echo 'blacklist ftdi_sio' > /etc/modprobe.d/ftdi.conf
 
-echo 'you should add your user to the usb group and reboot now, should be good to go ...'
+echo "** Removing old ftdi_sio driver (it's ok if it fails)"
+rmmod ftdi_sio
+
+echo "!! Run the following command as root, to add your user to the usb group"
+echo "useradd -G usb yourusernamehere"
