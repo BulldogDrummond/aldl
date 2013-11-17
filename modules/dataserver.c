3f0397e27c3a2b9f3f8ff153937decb1b1eeb852
resfilter <resfilter@resfilter.net>
2013-11-17 10:53:27 -0800

    stub file for new dataserver plugin

---
+++ modules/dataserver.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* local objects */
+#include "../error.h"
+#include "../aldl-io.h"
+#include "../loadconfig.h"
+#include "../useful.h"
+
+void *dataserver_init(void *aldl_in) {
+  aldl_conf_t *aldl = (aldl_conf_t *)aldl_in;
+  /* stub, complete me please ... */
+
+  return NULL;
+};
