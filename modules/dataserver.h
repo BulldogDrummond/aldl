bd823d79c3ce837fcc19462d694655fcad44b173
resfilter <resfilter@resfilter.net>
2013-11-18 16:06:20 -0800

    revise protocol bytes

--- modules/dataserver.h
+++ modules/dataserver.h
@@ -1,6 +1,29 @@
 
+/* -------- client to server command bytes */
 
-/* client to server command bytes */
-#define DATASERVER_COMM_SENDPKT 0x01
-#define DATASERVER_COMM_SENDHEADER 0x02
-#define DATASERVER_COMM_SENDSTATUS 0x03
+/* start sending a datastream */
+#define ALDL_COMM_STREAM 0x20
+
+/* send the stream definition header */
+#define ALDL_COMM_SENDHEADER 0x21
+
+/* send a server status byte */
+#define ALDL_COMM_SENDSTATUS 0x22
+
+/* -------- server to client command bytes */
+
+/* the 'command accepted' reply */
+#define ALDL_COMM_OK 0x10
+
+/* the 'command not accepted' reply */
+#define ALDL_COMM_ERR 0x11
+
+/* the server is full, please retry */
+#define ALDL_COMM_SERVERFULL 0x12
+
+/* prefix for a valid datastream */
+#define ALDL_COMM_OK 0x13
+
+/* the aldl stream is in a disconnected state */
+#define ALDL_COMM_DOWN 0x14
+
