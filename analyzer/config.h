2fcda26cd736b3c43e82bcda1c761ba15503a09d
resfilter <resfilter@resfilter.net>
2014-02-09 13:05:25 -0800

    specify analysis grid configuration

--- analyzer/config.h
+++ analyzer/config.h
@@ -1,2 +1,8 @@
 #define ANL_VERSION "0.5a"
 #define ANL_CONFIGFILE "/etc/aldl/analyzer.conf"
+
+/* configure analysis grids */
+#define GRID_RPM_RANGE 7000
+#define GRID_RPM_INTERVAL 250
+#define GRIP_MAP_RANGE 100
+#define GRID_MAP_INTERVAL 5
