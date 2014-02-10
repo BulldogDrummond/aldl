f7224dd808269b207a61dd957f7dcb60d4545aac
resfilter <resfilter@resfilter.net>
2014-02-09 16:12:42 -0800

    more work on knock analyzer

--- analyzer/analyzer.c
+++ analyzer/analyzer.c
@@ -10,6 +10,9 @@
 #include "loadconfig.h"
 #include "error.h"
 
+#define RPM_GRIDSIZE ( GRID_RPM_RANGE / GRID_RPM_INTERVAL )
+#define MAP_GRIDSIZE ( GRID_MAP_RANGE / GRID_MAP_INTERVAL )
+
 typedef struct _anl_fcell_t {
   float low,high;
   float avg;
@@ -27,6 +30,13 @@ typedef struct _anl_t {
 } anl_t;
 anl_t *anl_blm;
 
+typedef struct _anl_knock_t {
+  int t[RPM_GRIDSIZE][MAP_GRIDSIZE];
+  int ttl;
+  int prev;
+} anl_knock_t;
+anl_knock_t *anl_knock;
+
 typedef struct _anl_conf_t {
   int n_cols; /* number of columns in a log file */
   /* valid row specifier */
@@ -37,9 +47,11 @@ typedef struct _anl_conf_t {
   int blm_on; /* activate the blm analyzer */
   int blm_n_cells; /* number of blm cells */
   int blm_min_count; /* minimum number of counts for a valid cell */
+  /* knock analyzer */
+  int knock_on; /* activate knock counter */
   /* column identifiers */ 
   int col_timestamp, col_rpm, col_temp, col_lblm, col_rblm, col_cell;
-  int col_map, col_maf, col_cl, col_blm, col_wot;
+  int col_map, col_maf, col_cl, col_blm, col_wot, col_knock;
 } anl_conf_t;
 anl_conf_t *anl_conf;
 
@@ -53,6 +65,7 @@ void parse_line(char *line);
 int verify_line(char *line);
 
 void log_blm(char *line);
+void log_knock(char *line);
 
 int csvint(char *line, int f);
 float csvfloat(char *line, int f);
@@ -66,6 +79,10 @@ void post_calc();
 
 void print_results();
 void print_results_blm();
+void print_results_knock();
+
+int rpm_cell_offset(int value);
+int map_cell_offset(int value);
 
 int main(int argc, char **argv) {
   printf("**** aldlio offline log analyzer %s ****\n\n",ANL_VERSION);
@@ -104,6 +121,7 @@ void prep_anl() {
   anl_blm = malloc(sizeof(anl_t) * anl_conf->blm_n_cells);
   memset(anl_blm,0,sizeof(anl_t) * anl_conf->blm_n_cells);
 
+  /* config blm struct */
   if(anl_conf->blm_on) {
     int cell;
     anl_t *cdata;
@@ -121,6 +139,10 @@ void prep_anl() {
   stats->goodlines = 0;
   stats->badlines = 0;
   stats->skiplines = 0;
+
+  /* config knock struct */
+  anl_knock = malloc(sizeof(anl_knock_t));
+  memset(anl_knock,0,sizeof(anl_knock_t));
 };
 
 void parse_file(char *data) {
@@ -144,6 +166,43 @@ void parse_line(char *line) {
 
   /* bring in blm stuff */
   if(anl_conf->blm_on == 1) log_blm(line);
+
+  /* bring in knock stuff */
+  if(anl_conf->knock_on == 1) log_knock(line);
+};
+
+void log_knock(char *line) {
+  int kcount = csvint(line,anl_conf->col_knock);
+  if(kcount == anl_knock->prev) return; /* no new knock */
+  if(kcount < anl_knock->prev) {
+    /* either knock counter has rolled, or this is the initial value. */
+    /* FIXME we are discarding knock events during rollover ... */
+    anl_knock->prev = kcount; 
+    return;
+  };
+  /* at this point a knock event HAS been encountered */
+  int rpmcell = rpm_cell_offset(csvfloat(line,anl_conf->col_rpm));
+  int mapcell = map_cell_offset(csvfloat(line,anl_conf->col_map));
+  anl_knock->t[rpmcell][mapcell] += kcount; /* incr kcount */
+  anl_knock->prev = kcount; /* reset prev */
+  anl_knock->ttl += kcount; /* incr. ttl */
+};
+
+void print_results_knock() {
+  printf("\n**** KNOCK Analysis ****\n\n");
+  int maprow = 0;
+  int rpmrow = 0;
+  for(rpmrow=0;rpmrow<RPM_GRIDSIZE;rpmrow++) {
+    for(maprow=0;maprow<MAP_GRIDSIZE;maprow++) {
+      if(anl_knock->t[rpmrow][maprow] != 0) {
+        printf("RPM %i-%i @ MAP%i-%i: %i Counts\n",
+          rpmrow * GRID_RPM_INTERVAL, ( rpmrow + 1) * GRID_RPM_INTERVAL,
+          maprow * GRID_MAP_INTERVAL, ( maprow + 1) * GRID_MAP_INTERVAL,
+          anl_knock->t[rpmrow][maprow]);
+      };
+    };
+  };
+  printf("Total knock events: %.0f k\n",(float)anl_knock->ttl / 1000);
 };
 
 void log_blm(char *line) {
@@ -161,12 +220,8 @@ void log_blm(char *line) {
   };
 
   /* check CL/PE op */
-  if(anl_conf->valid_cl != -1 &&
-     csvint(line,anl_conf->col_cl) != anl_conf->valid_cl) return;
-  if(anl_conf->valid_blm != -1 && 
-     csvint(line,anl_conf->col_blm) != anl_conf->valid_blm) return;
-  if(anl_conf->valid_wot != -1 &&
-     csvint(line,anl_conf->col_wot) != anl_conf->valid_wot) return;
+  if(csvint(line,anl_conf->col_cl) != 1) return;
+  if(csvint(line,anl_conf->col_blm) != 1) return;
 
   /* point to cell index */
   anl_t *cdata = &anl_blm[cell];
@@ -223,6 +278,7 @@ void print_results() {
   printf("Skipped %i/%i unreliable records.\n",
       stats->skiplines,stats->goodlines);
   if(anl_conf->blm_on == 1) print_results_blm();
+  if(anl_conf->knock_on == 1) print_results_knock();
 };
 
 void print_results_blm() {
@@ -250,7 +306,7 @@ void print_results_blm() {
       if(cdata->blm.avg > 138) {
         printf("\t!!!! This cell is tuned too lean !!!!\n");
       };
-      if(cdata->blm.avg < 118) {
+      if(cdata->blm.avg < 110) {
         printf("\t!!!! This cell is tuned too rich !!!!\n");
       };
     } else {
@@ -303,10 +359,8 @@ void anl_load_conf(char *filename) {
   anl_conf->n_cols = configopt_int_fatal(dconf,"N_COLS",1,500);
   anl_conf->valid_min_time = configopt_int_fatal(dconf,"MIN_TIME",0,999999);
   anl_conf->valid_min_temp  = configopt_int_fatal(dconf,"MIN_TEMP",-20,99999);
-  anl_conf->valid_cl  = configopt_int_fatal(dconf,"VALID_CL",-1,1);
-  anl_conf->valid_blm  = configopt_int_fatal(dconf,"VALID_BLM",-1,1);
-  anl_conf->valid_wot = configopt_int_fatal(dconf,"VALID_WOT",-1,1);
   anl_conf->blm_on = configopt_int_fatal(dconf,"BLM_ON",0,1);
+  anl_conf->knock_on = configopt_int_fatal(dconf,"KNOCK_ON",0,1);
   if(anl_conf->blm_on == 1) {
     anl_conf->blm_n_cells = configopt_int_fatal(dconf,"N_CELLS",1,255);
     anl_conf->blm_min_count = configopt_int_fatal(dconf,"BLM_MIN_COUNTS",
@@ -314,6 +368,9 @@ void anl_load_conf(char *filename) {
     anl_conf->col_lblm = configopt_int_fatal(dconf,"COL_LBLM",0,500);
     anl_conf->col_cell = configopt_int_fatal(dconf,"COL_CELL",0,500);
     anl_conf->col_rblm = configopt_int_fatal(dconf,"COL_RBLM",0,500);
+  };
+  if(anl_conf->knock_on == 1) {
+    anl_conf->col_knock = configopt_int_fatal(dconf,"COL_KNOCK",0,500);
   };
   anl_conf->col_timestamp = configopt_int_fatal(dconf,"COL_TIMESTAMP",0,500);
   anl_conf->col_rpm = configopt_int_fatal(dconf,"COL_RPM",0,500);
@@ -323,5 +380,23 @@ void anl_load_conf(char *filename) {
   anl_conf->col_cl = configopt_int_fatal(dconf,"COL_CL",0,500);
   anl_conf->col_blm = configopt_int_fatal(dconf,"COL_BLM",0,500);
   anl_conf->col_wot = configopt_int_fatal(dconf,"COL_WOT",0,500);
+};
+
+int rpm_cell_offset(int value) {
+  if(value > GRID_RPM_RANGE) return GRID_RPM_RANGE / GRID_RPM_INTERVAL;
+  if(value < 0) return 0;
+  if(value < GRID_RPM_INTERVAL) return 0;
+  int cell = ((float)value/GRID_RPM_RANGE)*(GRID_RPM_RANGE/GRID_RPM_INTERVAL);
+  printf("RPM CELL %i\n",cell);
+  return cell;
+};
+
+int map_cell_offset(int value) {
+  if(value > GRID_MAP_RANGE) return GRID_MAP_RANGE / GRID_MAP_INTERVAL;
+  if(value < 0) return value;
+  if(value < GRID_MAP_INTERVAL) return 0;
+  int cell = ((float)value/GRID_MAP_RANGE)*(GRID_MAP_RANGE/GRID_MAP_INTERVAL);
+  printf("MAP CELL %i\n",cell);
+  return cell;
 };
 
